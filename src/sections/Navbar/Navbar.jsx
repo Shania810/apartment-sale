4762295f9fbae6f58d24442640804880945a73ea
Shania <shaniaantezana@gmail.com>
2023-08-23 20:05:08 -0300

    added changes in styles of the section Navbar

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     return (
         <nav>
             <FaHome size={50}/>
-            <a href='https://api.whatsapp.com/send?phone=47997290020'>Contato</a>
+            <a href='https://api.whatsapp.com/send?phone=47997290020'><b>Contato</b></a>
         </nav>
     )
 }
