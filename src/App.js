502f98894f9cdb0ed4e68600a713f4a36fe07f31
Shania <shaniaantezana@gmail.com>
2023-08-23 16:59:06 -0300

    Inserted section Bathrooms in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css'
 import { Navbar } from './sections/Navbar/Navbar'
 import { Main } from './sections/Main/Main'
-import { Rooms } from './sections/Rooms/Rooms';
+import { Rooms } from './sections/Rooms/Rooms'
+import { Bathrooms } from './sections/Bathrooms/Bathrooms'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
-      <Rooms/>
+      <Rooms />
+      <Bathrooms />
     </div>
   );
 }
