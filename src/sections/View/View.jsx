7b03424e4e9575551dc9a4601de497082c2874f8
Shania <shaniaantezana@gmail.com>
2023-08-23 19:19:37 -0300

    Created section View

---
+++ src/sections/View/View.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import view from '../../images/view.jpeg'
+
+export const View = () => {
+    return (
+        <div>
+            <h2>Vista</h2>
+            <img src={view} alt={view} />
+        </div>
+    )
+}
