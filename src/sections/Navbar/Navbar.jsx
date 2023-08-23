bc5bf802a96b21a0e1edd85810f3e69b93937109
Shania <shaniaantezana@gmail.com>
2023-08-23 15:03:21 -0300

    inserted style.css in section Navbar

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { FaHome } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa'
+import './style.css'
 
 export const Navbar = () => {
     return (
         <nav>
-            <FaHome />
+            <FaHome size={50}/>
             <a href='https://api.whatsapp.com/send?phone=47997290020'>Contato</a>
         </nav>
     )
