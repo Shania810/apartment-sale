968b02fe0ba2d59915cb27315185f4447b972239
Shania <shaniaantezana@gmail.com>
2023-08-23 15:28:04 -0300

    Created folder images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import { Navbar } from './sections/Navbar/Navbar';
+import './App.css'
+import { Navbar } from './sections/Navbar/Navbar'
+import {Main} from './sections/Main/Main'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
