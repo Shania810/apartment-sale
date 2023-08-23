b8f72f3e1840e26e721d40d6d760c7f53190ff2c
Shania <shaniaantezana@gmail.com>
2023-08-23 14:42:25 -0300

    created section NavBar and installed dependency

---
+++ src/sections/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaHome } from 'react-icons/fa';
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <FaHome />
+            <a href='https://api.whatsapp.com/send?phone=47997290020'>Contato</a>
+        </nav>
+    )
+}
+
